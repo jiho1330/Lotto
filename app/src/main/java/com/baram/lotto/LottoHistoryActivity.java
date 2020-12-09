8133476ffccf0f454ce46ad0422f0b983562681c
ledpear <tjsrb75@gmail.com>
2020-12-10 00:27:56 +0900

    Lotto History View 추가
    List View 추가 및 항목 추가 기능 버튼에 추가

--- app/src/main/java/com/baram/lotto/LottoHistoryActivity.java
+++ app/src/main/java/com/baram/lotto/LottoHistoryActivity.java
@@ -2,13 +2,54 @@ package com.baram.lotto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class LottoHistoryActivity extends AppCompatActivity {
+
+    Button btnLottoHistoryView;
+
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lotto_history);
+
+        // 역대 로또 정보 버튼
+        items = new ArrayList<String>();
+
+        // 어댑터 생성
+        adapter = new ArrayAdapter<String>(LottoHistoryActivity.this, android.R.layout.simple_list_item_1, items);
+
+        // 어댑터 설정
+        listView = (ListView) findViewById(R.id.LottoHistoryList);
+        listView.setAdapter(adapter);
+        //listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE); // 하나의 항목만 선택할 수 있도록 설정
+
+        btnLottoHistoryView = findViewById(R.id.btnLottoHistoryView);
+        btnLottoHistoryView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {   //여기서 클릭 시 행동을 결정
+                String text = "test";        // EditText에 입력된 문자열값을 얻기
+                items.add(text);                          // items 리스트에 입력된 문자열 추가
+                items.add("Sunday");
+                items.add("Monday");
+                items.add("Tuesday");
+                items.add("Wednesday");
+                items.add("Thursday");
+                items.add("Friday");
+                items.add("Saturday");
+                adapter.notifyDataSetChanged();           // 리스트 목록 갱신
+            }
+        });
     }
 }
