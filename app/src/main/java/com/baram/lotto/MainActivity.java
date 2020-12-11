f225d002fde20a3029acf58cbb3b948ac31b5b65
jiho1330 <cko13@naver.com>
2020-12-12 01:09:07 +0900

    불금!!

--- app/src/main/java/com/baram/lotto/MainActivity.java
+++ app/src/main/java/com/baram/lotto/MainActivity.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+
 public class MainActivity extends AppCompatActivity {
     Button btnLotto, btnMapView, btnLottoHistory;
-
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,5 +51,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(mIntent);
             }
         });
+
+        // 광고배너
+        AdView adView = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
 }
