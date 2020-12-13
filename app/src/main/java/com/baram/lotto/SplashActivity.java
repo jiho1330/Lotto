0ed3f73d4d9557b7177ff6d63a00321ce005e6fe
stafor94 <stafor@naver.com>
2020-12-13 23:26:28 +0900

    SplashActivity 추가

---
+++ app/src/main/java/com/baram/lotto/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.baram.lotto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplication(), MainActivity.class));    // 메인 Activity로 이동
+                SplashActivity.this.finish();   // 현재 Activity 종료
+            }
+        }, 2000);    // 2초 뒤 실행
+    }
+
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+
+}
